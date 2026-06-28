3c0c56ee99118ebca6590cedcc3daee0514c137e
akshatadk27 <akshatadk27@gmail.com>
2026-06-28 17:07:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,4 +142,5 @@ def backend_version():
 
 
 # -----------------------------------------
-app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
